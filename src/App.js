8f370c22c393a2a0fe30f7ca27651f062bf0d9c5
unknown <jordanmbush@gmail.com>
2018-01-26 16:08:00 -0700

    completed step 7

--- src/App.js
+++ src/App.js
@@ -10,22 +10,47 @@ class App extends Component {
       picture: '',
       name: '',
     };
+
+    this.updatePicture = this.updatePicture.bind(this);
+    this.updateName = this.updateName.bind(this);
+    this.addFriend = this.addFriend.bind(this);
   }
 
   updatePicture(value) {
-    this.setState({
-      picture: value,
-    });
+    this.setState({picture: value});
+  }
+  
+  updateName(value) {
+    this.setState({name: value});
   }
 
+  addFriend() {
+    const {friends, picture, name } = this.state;
+
+    let newFriends = friends.slice();
+    newFriends.push({ picture, name });
+
+    this.setState({friends:newFriends, picture: '', name: '',});
+  }
+
+
   render() {
+    console.log(this.state);
+    const friends = this.state.friends.map( (friend, i) => (
+      <div key={`friend-${i}-${friend.name}`}>
+        <img src={friend.picture}/>
+        <span>{friend.name}</span>
+      </div>
+    ));
+
     return (
       <div>
         <span>Picture:</span>
         <input onChange={e => this.updatePicture(e.target.value)} value={this.state.picture} />
         <span>Name:</span>
         <input onChange={e => this.updateName(e.target.value)} value={this.state.name} />
-        <button>Add Friend</button>
+        <button onClick={ () => this.addFriend()}>Add Friend</button>
+        {friends}
       </div>
     );
   }
