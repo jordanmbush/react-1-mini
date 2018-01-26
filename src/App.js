6f6fc1d821b1eaaf91095851b120dfe48193d9b0
unknown <jordanmbush@gmail.com>
2018-01-26 14:42:06 -0700

    initial commit - step 1 through 4

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Picture: <input />
+        Name: <input />
+        <button>Add Friend</button>
+      </div>
+    );
+  }
+}
+
+export default App;
